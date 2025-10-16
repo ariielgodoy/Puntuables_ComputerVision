65576695d311103c173ee9529af4738efd3f9367
ariielgodoy <165669544+ariielgodoy@users.noreply.github.com>
2025-10-17 01:01:15 +0200

    Add upper threshold for hand detection in Lab color space

--- VerVideo_Lab.m
+++ VerVideo_Lab.m
@@ -5,6 +5,7 @@ function VerVideo_Lab(fichero)
     f.WindowScrollWheelFcn = @(~,~) close(f); 
     umbral_A_minimo = 0.026; %Valor de a en Lab para contar pixel
     %como posible perteneciente a una mano
+    umbral_A_maximo = 0.09
     umbral_pixeles_minimos = 5000; %Número de pixeles minimos para decir que
     %hay una mano en el video.
 
@@ -16,7 +17,9 @@ function VerVideo_Lab(fichero)
         a_norm = double(a)/255;
         mascara = a_norm > umbral_A_minimo; %Buscar píxeles que superen el
         %umbral
-        vector_pixeles_posibles=sum(a_norm>umbral_A_minimo);
+        mascara_altos = a_norm < umbral_A_maximo;
+        mascara = mascara & mascara_altos;
+        vector_pixeles_posibles=sum(mascara);
         suma_vector_pixeles_posibles = sum(vector_pixeles_posibles);
         if (suma_vector_pixeles_posibles>5000)
             disp("Mano")
