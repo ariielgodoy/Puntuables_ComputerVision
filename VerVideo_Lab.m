b5fc018fbab8c1e90e97c5ec91a0347a37fe048a
ariielgodoy <juanarielgodoybaez750@gmail.com>
2025-10-16 16:09:09 +0200

    Nueva versión detector de manos

---
+++ VerVideo_Lab.m
@@ -0,0 +1,31 @@
+function VerVideo_Lab(fichero)
+    v = VideoReader(fichero);
+
+    f = figure;
+    f.WindowScrollWheelFcn = @(~,~) close(f); 
+    umbral_A_minimo = 0.026; %Valor de a en Lab para contar pixel
+    %como posible perteneciente a una mano
+    umbral_pixeles_minimos = 5000; %Número de pixeles minimos para decir que
+    %hay una mano en el video.
+
+    while hasFrame(v)
+        frame = readFrame(v);
+
+        labImage = rgb2lab(frame);        
+        a = labImage(:,:,2);  %Canal verde-rojo
+        a_norm = double(a)/255;
+        mascara = a_norm > umbral_A_minimo; %Buscar píxeles que superen el
+        %umbral
+        vector_pixeles_posibles=sum(a_norm>umbral_A_minimo);
+        suma_vector_pixeles_posibles = sum(vector_pixeles_posibles);
+        if (suma_vector_pixeles_posibles>5000)
+            disp("Mano")
+        else
+            disp("No hay mano")
+        end
+        a_norm(mascara) = a_norm(mascara) * 10;
+        imshow(a_norm), title('Canal a')
+
+        drawnow
+    end
+end
